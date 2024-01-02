f626297371e0850d4a885d4cedf39bf08c435ce0
Fahim-shad <fahimshad136@gmail.com>
2024-01-02 21:28:18 +0600

    tem added

---
+++ tem.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class tem{
+    public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.print("entered celcicus = ");
+int celcicus =input.nextInt();
+double F = 1.8* celcicus + 32;
+System.out.println(F);
+
+    }
+}
