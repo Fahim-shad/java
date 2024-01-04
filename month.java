45a96b46cb0fe2fcf7cba53c0cc3339a9b1ab0af
Fahim-shad <fahimshad136@gmail.com>
2024-01-04 21:00:36 +0600

    month added

---
+++ month.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+public class month{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int month =input.nextInt();
+        if(month==1)
+        {
+            System.out.println("31");
+        }
+
+        else if(month==2)
+        {
+            System.out.println("28");
+        }
+        else if(month==3)
+        {
+            System.out.println("31");
+        }
+        else if(month==4)
+        {
+            System.out.println("30");
+        }
+        else if(month==5)
+        {
+            System.out.println("31");
+        }
+        
+        else if(month==6)
+        {
+            System.out.println("30");
+        }
+        else if(month==7)
+        {
+            System.out.println("31");
+        }
+        else if(month==8)
+        {
+            System.out.println("31");
+        }
+
+        else if(month==9)
+        {
+            System.out.println("30");
+        }
+        else if(month==10)
+        {
+            System.out.println("31");
+        }
+
+        else if(month==11)
+        {
+            System.out.println("30");
+        }
+        else if(month==12)
+        {
+            System.out.println("31");
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
