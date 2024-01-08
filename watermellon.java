48264b736e8f4b12bd7995e972d3226c945e6256
Fahim-shad <fahimshad136@gmail.com>
2024-01-09 00:10:28 +0600

    watermellon added

---
+++ watermellon.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class watermellon{
+    public static void main(String[] args){
+Scanner input =new Scanner(System.in);
+int num =input.nextInt();
+ if(num%2==0)
+  {
+    System.out.println("YES");
+  }
+else{
+    System.out.println("NO");
+}
+
+
+
+    }
+}
