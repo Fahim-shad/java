0fc8078cf924169f8ca5822d279fb9943fe3addf
Fahim-shad <fahimshad136@gmail.com>
2024-01-17 20:49:21 +0600

    calculator function added

---
+++ calculatorfun.java
@@ -0,0 +1,37 @@
+public class calculatorfun {
+    static int sum(int x, int y)
+    {
+       return x+y;
+       
+
+    }
+static int sub(int x, int y)
+    {
+       return x-y;
+       
+
+    }
+static int div(int x, int y)
+    {
+       return x/y;
+       
+
+    }
+
+static int mul(int x, int y)
+    {
+       return x*y;
+       
+
+    }
+
+    public static void main(String[] args) {
+    System.out.println(sum(6,3));
+    System.out.println(sub(6,3));
+    System.out.println(div(6,3));
+    System.out.println(mul(10,2));
+ } 
+
+
+}
+
